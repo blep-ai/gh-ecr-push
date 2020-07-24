613f27b0b74548cdcb69b9c3f81860cfd5c4966c
Allie <azhang@users.noreply.github.com>
2020-07-24 01:25:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const core = require('@actions/core');
 
 const AWS_ACCESS_KEY_ID = core.getInput('access-key-id', { required: true });
 const AWS_SECRET_ACCESS_KEY = core.getInput('secret-access-key', { required: true });
-const image = core.getInput('image', { required: true });
-const localImage = core.getInput('local-image') || image;
+const images = core.getInput('images', { required: true }).split(",");
+const tag = core.getInput('tag') || "";
 const awsRegion = core.getInput('region') || process.env.AWS_DEFAULT_REGION || 'us-east-1';
 const direction = core.getInput('direction') || 'push';
 
@@ -29,13 +29,18 @@ const accountData = run(`aws sts get-caller-identity --output json`);
 const awsAccountId = JSON.parse(accountData).Account;
 
 if (direction === 'push') {
-    console.log(`Pushing local image ${localImage} to ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
-    run(`docker tag ${localImage} ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
-    run(`docker push ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
+    images.forEach((images) => {
+        console.log(`Pushing local image ${image} to ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
+        run(`docker tag ${image} ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
+        run(`docker tag ${image} ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}${tag}`);
+        run(`docker push ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
+    })
 } else if (direction == 'pull') {
-    console.log("Pulling ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image} to ${localImage}");
-    run(`docker pull ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
-    run(`docker tag ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image} ${localImage} `);
+    images.forEach((images) => {
+        console.log("Pulling ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image} to ${localImage}");
+        run(`docker pull ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
+        run(`docker tag ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image} ${image} `);
+    })
 } else {
     throw new Error(`Unknown direction ${direction}`);
 }
